84b03fccf85787c032dd5801b5335247f2bea208
박준민PJM <3633qkrtk@naver.com>
2021-09-18 13:53:39 +0000

    [BOJ] 1110_1:  더하기 사이클

---
+++ BOJ/1110_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num=0;//입력숫자
+	int ten=0;//십의 자리
+	int one=0;//일의 자리
+	int sum=0, sum1=-1;//1차,2차 합
+	int result=0;//결과값
+	int cnt=0;//사이클
+	
+	scanf("%d", &num);//입력을 한다
+	result=num;
+	
+	while(sum1 != result)
+	{
+		ten=num/10;//십의 자리 구하기
+	    one=num%10;//일의 자리 구하기
+		sum=(ten+one)%10;//십의 자리와 일의 자리를 더한 후, 10으로 나눠 일의자리를 구한다.
+		sum1=(one*10)+sum;//num의 1의자리에 10을 곱하고 위의 값을 더한다
+		num=sum1;//sum1이 num이 된다
+		cnt++;//카운트에 1추가
+		
+	}
+	
+	printf("%d\n", cnt);
+	return 0;
+}
