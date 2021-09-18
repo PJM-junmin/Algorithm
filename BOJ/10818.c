7878e379c2ed2bfca16d1d7e5cba076d69c6c9ca
박준민PJM <3633qkrtk@naver.com>
2021-09-18 13:06:38 +0000

    First commit

---
+++ BOJ/10818.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int Num = 0, input = 0, max = 0, min = 0;
+	scanf("%d", &Num);
+	
+	int arr[Num];
+	
+	for(int i = 0; i < Num; i++)
+	scanf("%d", &arr[i]);
+	
+	max = min = arr[0];
+	for(int j = 0; j < Num; j++)
+	{
+		if(max < arr[j])
+			max = arr[j];
+		if(min > arr[j])
+			min = arr[j];
+	}
+	
+	printf("%d %d\n", min, max);
+	return 0;
+}
