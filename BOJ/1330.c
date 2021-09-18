35806a6e761d2c5d23be0f6ada56696ad3d67c56
박준민PJM <3633qkrtk@naver.com>
2021-09-18 13:55:58 +0000

    [BOJ] 1330:  두 수 비교하기

---
+++ BOJ/1330.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int A = 0, B = 0;
+	scanf("%d %d", &A, &B);
+	
+	if(A > B)
+		printf(">");
+	if(A < B)
+		printf("<");
+	if(A == B)
+		printf("==");
+	return 0;
+}
